4ee5c3611f7191f39ea9d88c05ca2e961e69a366
Anish Chaudhary <w0848247@myscc.ca>
2024-10-02 16:28:41 -0400

    Done with step 6 and step 7.

--- src/app/Services/football.service.ts
+++ src/app/Services/football.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {Football} from "../Shared/Models/Football";
+import {Observable, of} from "rxjs";
+import {footballs} from "../Shared/mockFootball";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,7 @@ import { Injectable } from '@angular/core';
 export class FootballService {
 
   constructor() { }
+  getFootballs():Observable<Football[]> {
+    return of(footballs);
+  }
 }
--- src/app/football-list/football-list.component.ts
+++ src/app/football-list/football-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {Football} from "../Shared/Models/Football";
 import {FootballListItemComponent} from "../football-list-item/football-list-item.component";
 import {NgClass, NgForOf} from "@angular/common";
+import {FootballService} from "../Services/football.service";
 
 @Component({
   selector: 'app-football-list',
@@ -12,13 +13,9 @@ import {NgClass, NgForOf} from "@angular/common";
   styleUrl: './football-list.component.css'
 })
 export class FootballListComponent {
-  football1: Football = {playerName:"Messi", playerPosition:"SecondStriker",playerJerseyNumber:10,playerAge:37,isplayertrophies: true};
-  football2: Football = {playerName:"Ronaldo", playerPosition:"CentreForward",playerJerseyNumber:7,playerAge:39,isplayertrophies: true};
-  football3: Football = {playerName:"Ter stegan", playerPosition:"Goalkepper",playerJerseyNumber:1,playerAge:33,isplayertrophies: false};
-  football4: Football = {playerName:"Di maria", playerPosition:"Attacking Midfielder",playerJerseyNumber:8,playerAge:38,isplayertrophies: true};
-  football5: Football = {playerName:"Van dyke", playerPosition:"Defender",playerJerseyNumber:3,playerAge:32,isplayertrophies: false};
-  football6: Football = {playerName:"Joshua Kimmich", playerPosition:"Defensive Midfielder",playerJerseyNumber:8,playerAge:29,isplayertrophies: false};
+  footballs:Football[] = [];
 
-  footballlist: Football [] = [this.football1, this.football2, this.football3, this.football4, this.football5, this.football6];
+  constructor(private footballService : FootballService) {
+  }
 
 }
