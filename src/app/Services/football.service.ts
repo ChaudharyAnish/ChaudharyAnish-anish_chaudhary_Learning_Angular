712de53490ab53ad780e97863f8a1d149551e1b9
Anish Chaudhary <w0848247@myscc.ca>
2024-10-02 16:55:36 -0400

    Also completed the 8 step and 9 step.

--- src/app/Services/football.service.ts
+++ src/app/Services/football.service.ts
@@ -7,9 +7,31 @@ import {footballs} from "../Shared/mockFootball";
   providedIn: 'root'
 })
 export class FootballService {
+  private footballList : Football[] = footballs;
 
   constructor() { }
   getFootballs():Observable<Football[]> {
     return of(footballs);
+  }
+  addFootball(newFootball:Football): Observable<Football[]>{
+    this.footballList.push(newFootball)
+    return of(this.footballList);
+  }
+
+  updateFootball(updateFootball:Football): Observable<Football[]>{
+    const index = this.footballList.findIndex(foot => foot.playerName === updateFootball.playerName);
+    if (index !== -1){
+      this.footballList[index] = updateFootball;
+    }
+    return of(this.footballList);
+  }
+
+  deleteFootball(deleteplayerName: string): Observable<Football[]>{
+    this.footballList = this.footballList.filter(foot => foot.playerName !== deleteplayerName);
+    return of (this.footballList);
+  }
+  getFootball(readplayerName:string): Observable<Football | undefined>{
+    const football = this.footballList.find(foot => foot.playerName === readplayerName);
+    return of(football);
   }
 }
