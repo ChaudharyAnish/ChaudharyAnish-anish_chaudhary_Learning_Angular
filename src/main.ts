6f8b0f2192e79255b475d9d13f0a47d9da824313
Anish Chaudhary <w0848247@myscc.ca>
2024-10-09 17:48:56 -0400

    now, I have completed step 3, step 4 and step 5.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import {provideRouter, Routes} from "@angular/router";
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {FootballListItemComponent} from "./app/football-list-item/football-list-item.component";
+import {FootballListComponent} from "./app/football-list/football-list.component";
+import * as path from "node:path";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  {path:'', redirectTo: '/footballs', pathMatch: 'full'},
+  {path:'footballs', component: FootballListComponent},
+  {path: 'footballs/:playerName', component: FootballListItemComponent},
+]
+bootstrapApplication(AppComponent,{providers: [provideRouter(routes)]})
+  .then(r => console.log('Bootstrap successfull'));
+
